24d8d944004a31fd0f90a52f7a271564aacda522
torrayne <daniel@atwood.io>
2024-05-24 19:39:46 -0500

    remove debug log

--- netlify/functions/formailer/main.go
+++ netlify/functions/formailer/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/torrayne/formailer"
 	"github.com/torrayne/formailer/handlers"
 )
 
 func main() {
-	fmt.Println("HIT FORMAILER")
 	contact := formailer.Form{Name: "Contact"}
 	contact.AddEmail(formailer.Email{
 		To:      "rayne@atwood.io",
